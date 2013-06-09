6e325e6bebf528997a384fa06c7ac8a4918afc0a
Orest Voloshchuk <orest.v@gmail.com>
2013-06-09 17:02:33 +0300

    FIX: attendees now sorted by all text fields

--- model.py
+++ model.py
@@ -2,7 +2,7 @@
 import re
 from flask.ext.pymongo import PyMongo
 from flask.ext.pymongo import ASCENDING, DESCENDING
-
+import locale
 
 def get_word_regexp(word):
     return re.compile('^%s' % word, re.IGNORECASE)
@@ -19,6 +19,12 @@ def intersect_attendees(a, b):
     result = filter(lambda item: item['_id'] in ids, a + b)
     return result
 
+def compare_attendees(a1, a2):
+    for field in ['lastname', 'firstname', 'middlename']:
+        compare_result = locale.strcoll(a1[field], a2[field])
+        if compare_result:
+            return compare_result
+    return 0
 
 def find_attendees(db, search_term, search_by_city = False):
     words = search_term.split(' ')
