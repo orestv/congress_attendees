f2cd3b01238a3b1a56a22b64a916192bd3961de4
Orest Voloshchuk <orest.v@gmail.com>
2013-07-13 00:20:52 +0300

    ADD: separated code handling different request methods

--- site_main.py
+++ site_main.py
@@ -14,8 +14,6 @@ app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'congress'
 mongo = PyMongo(app)
-print mongo
-
 
 def get_db():
     return mongo.db
@@ -39,8 +37,6 @@ def find_attendees_by_word(search_term):
     attendee_count = len(attendees)
     attendees.sort(cmp = model.compare_attendees)
     attendees = attendees[:30]
-    # for attendee in attendees:
-    #     attendee['_id'] = str(attendee['_id'])
     result = {'count': attendee_count, 'attendees': attendees}
     return result
 
@@ -49,10 +45,22 @@ def find_attendee_by_id(id):
     if not cursor:
         return {}
     attendee = cursor[0]
-    # attendee['_id'] = str(attendee['_id'])
     return attendee
 
-@app.route('/attendees', methods=['GET', 'POST', 'PUT'])
+@app.route('/attendees', methods=['PUT'])
+def update_attendee():
+    id = request.args.get('id', None)
+    events = request.args.get('events', None)
+    if events is not None:
+        events = events.split(',') if events else []
+        model.set_attendee_events(get_db(), id, events)
+    registered = request.args.get('registered', None)
+    if registered is not None:
+        registered = bool(registered)
+        model.set_attendee_registered(get_db(), id, registered)
+    return json.dumps({})
+
+@app.route('/attendees', methods=['GET'])
 def attendees():
     result = {}
     search_term = request.args.get('s', None)
@@ -60,25 +68,14 @@ def attendees():
         result = find_attendees_by_word(search_term)
     id = request.args.get('id', None)
     if id:
-        print 'Request method: ', request.method
-        if request.method == 'PUT':
-            events = request.args.get('events', None)
-            if events is not None:
-                events = events.split(',') if events else []
-                model.set_attendee_events(get_db(), id, events)
-            registered = request.args.get('registered', None)
-            if registered is not None:
-                registered = bool(registered)
-                model.set_attendee_registered(get_db(), id, registered)
-        else:
-            attendee = find_attendee_by_id(id)
-            events_db = model.get_events(get_db())
-            events = []
-            for event in events_db:
-                if 'limit' in event:
-                    event['attendees'] = model.get_event_attendees_count(get_db(), event['_id'])
-                events.append(event)
-            result = {'attendee': attendee, 'events': events}
+        attendee = find_attendee_by_id(id)
+        events_db = model.get_events(get_db())
+        events = []
+        for event in events_db:
+            if 'limit' in event:
+                event['attendees'] = model.get_event_attendees_count(get_db(), event['_id'])
+            events.append(event)
+        result = {'attendee': attendee, 'events': events}
     return json.dumps(result, default=json_util.default)
 
 app.run(debug=True)
